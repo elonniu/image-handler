cd4c580e410844dcaa52d4cdbbc78d32c9244ea8
Elon Niu <elonniu@amazon.com>
2023-06-10 20:45:51 +0800

    improved arch

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,15 +1,47 @@
-import {ApiHandler} from "sst/node/api";
-import {Lambda, SQS} from "aws-sdk";
-import {Function} from "sst/node/function";
+import {S3, SQS} from "aws-sdk";
 import {Queue} from "sst/node/queue";
 import {jsonParseRecursive} from "sst-helper";
+import {int} from "aws-sdk/clients/datapipeline";
+import sharp from "sharp";
+import axios from "axios";
+import {Bucket} from "sst/node/bucket";
 
-export const handler = ApiHandler(async (_evt) => {
+export const handler = async (event: any, context: any) => {
 
-    const body = JSON.parse(_evt.body || "{}");
+    console.log(event);
+
+    if (event.body) {
+        return await apiHandler(event, context);
+    }
+
+    const {Url, Key, Width, Height, Quality, Records} = event;
+
+    // From Event
+    if (Url) {
+        const image = await compress(Url, Key, Width, Height, Quality);
+        console.log(image);
+
+        return {};
+    }
+
+    // From SQS
+    if (Records) {
+        for (const record of Records) {
+            const {body} = record;
+            const {Url, Key, Width, Height, Quality} = JSON.parse(body);
+            const image = await compress(Url, Key, Width, Height, Quality);
+            console.log(image);
+        }
+    }
+
+    return {};
+}
+
+export const apiHandler = async (_evt: any, context: any) => {
+    const body = JSON.parse(_evt.body || "[]");
     const {Url, InvocationType, Width, Height, Quality} = body;
 
-    // generate unique filename
+    // generate unique filename4
     const Uuid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     const Key = `${Uuid}-${Width}x${Height}.jpg`;
 
@@ -26,13 +58,13 @@ export const handler = ApiHandler(async (_evt) => {
 
     const event = {InvocationType, Url, Key, Width, Height, Quality};
 
-    // InvocationType must be in ["Event", "RequestResponse", "Queue"]
-    if (!["Event", "RequestResponse", "Queue"].includes(InvocationType)) {
+    // InvocationType must be in ["RequestResponse", "Queue"]
+    if (!["RequestResponse", "Queue"].includes(InvocationType)) {
         return {
             statusCode: 400,
             contentType: "application/json",
             body: JSON.stringify({
-                error: "InvocationType must be in ['Event', 'RequestResponse', 'Queue']",
+                error: "InvocationType must be in ['RequestResponse', 'Queue']",
             }, null, 2),
         };
     }
@@ -42,30 +74,124 @@ export const handler = ApiHandler(async (_evt) => {
         if (InvocationType === "Queue") {
             // put record into sqs queue
             const sqs = new SQS();
-            const result = await sqs.sendMessage({
+            // send message batch
+            const result = await sqs.sendMessageBatch({
                 QueueUrl: Queue.Queue.queueUrl,
-                MessageBody: JSON.stringify(event),
+                Entries: [
+                    {
+                        Id: "1",
+                        MessageBody: JSON.stringify(event),
+                    },
+                    {
+                        Id: "2",
+                        MessageBody: JSON.stringify(event),
+                    },
+                ]
             }).promise();
 
-            return {event, result};
+            return {result};
         }
 
-        // use aws sdk invoke lambda function asynchronously
-        const lambda = new Lambda();
-        const result = await lambda.invoke({
-            FunctionName: Function.ImageHandle.functionName,
-            InvocationType,
-            Payload: JSON.stringify(event)
-        }).promise();
+        // use aws sdk invoke lambda function synchronously
+        const result = await compress(Url, Key, Width, Height, Quality);
 
         jsonParseRecursive(result);
         jsonParseRecursive(result);
 
-        return {event, result};
+        return {result};
 
     } catch (e) {
-        return {event, error: e.message};
+        return {
+            statusCode: 400,
+            contentType: "application/json",
+            body: JSON.stringify({
+                error: e.message
+            }, null, 2),
+        };
     }
 
 
-});
+}
+
+
+export const compress = async (url: string, Key: string, Width: int, Height: int, Quality: int) => {
+
+    // record getimage latency
+    const startTime = new Date().getTime();
+    const originalImage = await getImage(url);
+    const endTime = new Date().getTime();
+
+    const downloadImageLatencyMS = endTime - startTime;
+
+    // get response.data size in mb
+    const originalByteLength = originalImage.byteLength;
+    //record compress latency
+    const startTime2 = new Date().getTime();
+
+    const originalImageMeta = await sharp(originalImage).metadata();
+
+    const compressedImage = await sharp(originalImage)
+        .resize(Width, Height, {
+            fit: 'inside',
+            withoutEnlargement: true,
+        })
+        .jpeg({quality: Quality})
+        .toBuffer();
+
+    const endTime2 = new Date().getTime();
+    const compressImageLatencyMS = endTime2 - startTime2;
+
+    const compressedByteLength = compressedImage.byteLength;
+
+    const imageUrl = await uploadImage(compressedImage, Key);
+
+    // get compress ratio
+    const compressRatio = compressedByteLength / originalByteLength;
+
+    // get image resolution
+    const compressedImageMeta = await sharp(compressedImage).metadata();
+
+    // make beforeByteLength afterByteLength to mb
+    const originalMB = (originalByteLength / 1024 / 1024).toFixed(2) + ' MB';
+    const compressedMB = (compressedByteLength / 1024 / 1024).toFixed(2) + ' MB';
+
+    return {
+        downloadImageLatencyMS,
+        compressImageLatencyMS,
+        originalByteLength,
+        compressedByteLength,
+        compressRatio,
+        originalMB,
+        compressedMB,
+        originalImageMeta,
+        compressedImageMeta,
+        imageUrl
+    };
+}
+
+// create getImage function
+export const getImage = async (url: string) => {
+    const response = await axios.get(url, {
+        responseType: 'arraybuffer'
+    });
+
+    return response.data;
+}
+
+//create uploadImage function
+export const uploadImage = async (imageBuffer: Buffer, Key: string) => {
+    const s3 = new S3();
+    const upload = s3.upload({
+        Bucket: Bucket.Bucket.bucketName,
+        Key,
+        Body: imageBuffer,
+        ContentType: 'image/jpeg',
+        ContentDisposition: 'inline',
+    })
+    await upload.promise();
+    return s3.getSignedUrl('getObject', {
+        Bucket: Bucket.Bucket.bucketName,
+        Key,
+        Expires: 60 * 60 * 24 * 365 * 10,
+    });
+}
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,23 +1,22 @@
 import {Api, Bucket, Function, Queue, StackContext} from "sst/constructs";
 import {lambdaUrl, s3Url} from "sst-helper";
-import {ServicePrincipal} from "aws-cdk-lib/aws-iam";
 import {Duration} from "aws-cdk-lib";
 
 export function API({stack, app}: StackContext) {
     // create bucket by sst v2
     const bucket = new Bucket(stack, "Bucket", {});
 
-    // create lambda function sst v2
-    const imageHandle = new Function(stack, "ImageHandle", {
-        handler: "packages/functions/src/imageHandle.handler",
-        bind: [bucket],
-        runtime: "nodejs16.x",
-        memorySize: 4048,
-        timeout: 50,
-        currentVersionOptions: {
-            provisionedConcurrentExecutions: 1,
+    const apiLambda = new Function(stack, "apiLambda", {
+            handler: "packages/functions/src/lambda.handler",
+            bind: [bucket],
+            runtime: "nodejs16.x",
+            memorySize: 4048,
+            timeout: 50,
+            currentVersionOptions: {
+                provisionedConcurrentExecutions: 1,
+            }
         }
-    });
+    );
 
     // create queue by sst 2
     const queue = new Queue(stack, "Queue", {
@@ -28,7 +27,7 @@ export function API({stack, app}: StackContext) {
         },
         consumer: {
             cdk: {
-                function: imageHandle,
+                function: apiLambda,
                 eventSource: {
                     batchSize: 1,
                     reportBatchItemFailures: false,
@@ -38,16 +37,7 @@ export function API({stack, app}: StackContext) {
 
     });
 
-    const apiLambda = new Function(stack, "apiLambda", {
-            handler: "packages/functions/src/lambda.handler",
-            bind: [imageHandle, queue],
-        }
-    );
-
-    imageHandle.bind([queue]);
-
-    // aws cli to invoke lambda function
-    // aws lambda invoke --function-name imageHandle --payload '{"key": "test.jpg"}' response.json
+    apiLambda.bind([queue]);
 
     const api = new Api(stack, "api", {
         routes: {
@@ -55,16 +45,9 @@ export function API({stack, app}: StackContext) {
         },
     });
 
-    // create permission for lambda invoke lambda by cdk
-    imageHandle.addPermission("invokeByLambda", {
-        principal: new ServicePrincipal("lambda.amazonaws.com"),
-        sourceArn: apiLambda.functionArn
-    });
-
     stack.addOutputs({
         ApiEndpoint: api.url,
         lambda: lambdaUrl(apiLambda, app),
-        imageHandle: lambdaUrl(imageHandle, app),
         bucket: s3Url(bucket, app)
     });
 }
